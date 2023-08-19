fb3111340a533f7b7a5063c52289d258bb38fbe6
pyatkov-vladimir <141109125+pyatkov-vladimir@users.noreply.github.com>
2023-08-19 22:31:11 +1000

    Homework2.py

---
+++ Homework2.py
@@ -0,0 +1,80 @@
+import math
+
+item_1 = 28 #1
+item_2 = 13#2
+result_sum = item_1 + item_2#3
+print(result_sum)#4
+
+result_subtr = item_1-item_2 #5
+print(result_subtr) #6
+
+result_multi = item_1*item_2 #7
+print(result_multi) #8
+
+result_exp = item_1**item_2  #9
+print(result_exp) #10
+
+result_m_exp = math.pow(item_1,item_2) #11
+print(result_m_exp) #12
+
+result_s_root = item_1**0.5#13
+print(result_s_root) #14
+
+result_m_s_root = math.sqrt(item_1) #15
+print(result_m_s_root) #16
+
+result_mp_s_root = math.pow (item_2,0.5)#17
+print(result_mp_s_root) #18
+
+
+item_1 = 211 #19
+item_2 = 12 #20
+
+result_division = item_1/item_2 #21
+print(result_division) #22
+
+result_m_floor = round(int(result_division),1) #23
+print(result_m_floor) #24
+
+result_m_ceil = round(result_division) #25
+print(result_m_ceil) #26
+
+result_int = round(int(result_division)) #27
+print(result_int) #28
+
+result_no_division_loss = item_1//item_2 #29
+print(result_no_division_loss) #30
+
+result_division_loss = item_1 % item_2  #31
+print(result_division_loss)#32
+
+item_3 = 129 #33
+item_3 += 10 #34
+print(item_3)#35
+item_3 -=5 #36
+print(item_3) #37
+item_3 *=3#38
+print(item_3)#39
+item_3 /=2 #40
+print(item_3) #41
+item_3 **=2 #42
+print(item_3) #43
+item_3 **=0.5 #44
+print(item_3)#45
+item_3 = item_3% 2#46
+print(item_3)#47
+b_item_t = True #48
+b_item_f = False #49
+b_item_result_sum = b_item_t+b_item_f #50
+print(b_item_result_sum) #51
+b_item_result_subtr =b_item_t-b_item_f#52
+print(b_item_result_subtr)#53
+b_item_result_multi = b_item_t*b_item_f#54
+print(b_item_result_multi)#55
+b_item_result_division = b_item_t/b_item_f #56
+print(b_item_result_division)#57
+b_item_1_int = int(b_item_t)#58
+print(b_item_1_int)#59
+b_item_2_int = int(b_item_f)
+print(b_item_2_int)
+
